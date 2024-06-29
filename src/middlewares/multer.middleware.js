84df5478c9c6959deb950eea3ddb144a50e3581d
anchitshahare <arshahare1310@gmail.com>
2024-06-29 17:51:40 +0530

    add multer and cloudinary files for file upload

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,17 @@
+// multer documentation to read more: https://github.com/expressjs/multer#readme
+
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.originalname)
+    }
+  })
+  
+  export const upload = multer({ 
+    storage,
+})
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs' // nodejs method to read, write, remove, etc. files
+
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+});
+
+// method to upload file on cloudinary
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        // upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been successfully uploaded
+        console.log("File uploaded on cloudinary ", response.url);
+        return response 
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload failed
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
